72c23c3a1aefba6aa09d363d1fb07f997aeffcd8
lcarbonn <laurent.carbonnaux@gmail.com>
2023-12-08 10:09:04 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>{{ version }}</span>
+  <!-- <div class="d-flex flex-column justify-content-between min-vh-100">
+    <div>
+      <BaseNavBar  @filter-cards="filterCards" @search-cards="searchCards"></BaseNavBar>
+      <BContainer fluid>
+        <NuxtPage />
+      </BContainer>
+    </div>
+    <div class="d-flex flex-column justify-content-end">
+      <BaseFooter :appVersion="version"></BaseFooter>
+      <BaseSnackbar></BaseSnackbar>
+    </div>
+  </div> -->
+</template>
+<script setup lang="ts">
+
+  // imports
+  import { version } from '~/package.json';
+
+  // nuxt cycle hooks
+  onMounted(() => {
+    console.log("appVersion:",version)
+  })
+
+</script>
