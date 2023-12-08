8311cd7c40b02ddd630e086297767a787f3bea3d
lcarbonn <laurent.carbonnaux@gmail.com>
2023-12-08 10:16:06 +0100

    add app conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app:{
+    head: {
+      title: 'My Bill Airtable',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'My Bill Airtable' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    },
+  },
   modules: ['@bootstrap-vue-next/nuxt'],
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
